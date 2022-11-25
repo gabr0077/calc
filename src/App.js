f81183117372db60a37605f23288b0be6de55737
gabr0077 <gabr007@hotmail.com>
2022-11-25 16:04:09 +0000

    clac app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,77 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import { evaluate } from "mathjs";
+
 
 function App() {
+
+  const [calc, setCalc] = useState("")
+  const [result, setResult] = useState("");
+  const ops = ['/', '*', '-', '+', '.'];
+
+  const updateCalc = value => {
+
+    if (
+      (ops.includes(value) && calc === '') ||
+      (ops.includes(value) && ops.includes(calc.slice(-1))
+      )) {
+      setResult(result + value)
+    }
+
+
+
+    setCalc(calc + value);
+
+  }
+
+  const createdigits = () => {
+    const digits = []
+
+    for (let i = 1; i < 10; i++) {
+      digits.push(
+        <button onClick={() => updateCalc(i.toString())()} key={i}>{i}</button>
+      )
+
+    }
+    return digits;
+
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Ap">
+      <div className="calculator">
+        <div className="display">
+          {/* {result ? <span>(0)</span> : ''} */}
+          {/* {calc || "0"} */}
+          <p> {calc}</p>
+
+
+        </div>
+        <div className="operators">
+
+          <button onClick={() => updateCalc('/')}>/
+          </button>
+          <button onClick={() => updateCalc('*')}>*
+          </button>
+          <button onClick={() => updateCalc('+')}>+</button>
+          <button onClick={() => updateCalc('-')}>-</button>
+
+          <button onClick={() => setCalc("")}>C</button>
+          <div className="digits">
+            {createdigits()}
+            <button onClick={() => updateCalc('0')}>0</button>
+            <button onClick={() => updateCalc('.')}>.</button>
+            <button onClick={() => setCalc(evaluate(calc))}>=</button>
+
+
+
+          </div>
+
+
+
+        </div>
+      </div>
     </div>
-  );
+  )
 }
+
 
 export default App;
